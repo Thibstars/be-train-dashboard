01e786ac9f5cb2ff77a300c123dbad6eef87b30d
Thibstars <thibault.helsmoortel@gmail.com>
2023-08-24 23:52:59 +0200

    Improve StationPanel

--- src/main/java/com/github/thibstars/btsd/desktop/StationPanel.java
+++ src/main/java/com/github/thibstars/btsd/desktop/StationPanel.java
@@ -1,7 +1,8 @@
 package com.github.thibstars.btsd.desktop;
 
 import com.github.thibstars.btsd.irail.model.Station;
-import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 /**
@@ -10,10 +11,13 @@ import javax.swing.JPanel;
 public class StationPanel extends JPanel {
 
     public StationPanel(Station station) {
-        JLabel lblId = new JLabel(station.id());
-        JLabel lblName = new JLabel(station.name());
-        JLabel lblStandardName = new JLabel(station.standardName());
-        JLabel lblCoordinates = new JLabel("(" + station.locationX() + ", " + station.locationY() + ")");
+        setLayout(new GridLayout(2, 2));
+        setBorder(BorderFactory.createTitledBorder("Station - " + station.name()));
+
+        CaptionedLabel lblId = new CaptionedLabel("Id:", station.id());
+        CaptionedLabel lblName = new CaptionedLabel("Name:", station.name());
+        CaptionedLabel lblStandardName = new CaptionedLabel("Default name:", station.standardName());
+        CaptionedLabel lblCoordinates = new CaptionedLabel("Coordinates:", "(" + station.locationX() + ", " + station.locationY() + ")");
 
         add(lblId);
         add(lblName);
