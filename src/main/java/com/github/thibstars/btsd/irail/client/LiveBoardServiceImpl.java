91e66ed475d277564d226814aef5a67c33c60d6a
Thibstars <thibault.helsmoortel@gmail.com>
2023-08-24 23:20:36 +0200

    Implement LiveBoardService

---
+++ src/main/java/com/github/thibstars/btsd/irail/client/LiveBoardServiceImpl.java
@@ -0,0 +1,53 @@
+package com.github.thibstars.btsd.irail.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.thibstars.btsd.irail.exceptions.ClientException;
+import com.github.thibstars.btsd.irail.model.LiveBoard;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * @author Thibault Helsmoortel
+ */
+public class LiveBoardServiceImpl implements LiveBoardService {
+
+    private static final String URL = "https://api.irail.be/liveboard/?id=${id}&arrdep=departure&lang=en&format=json&alerts=false";
+
+    private final OkHttpClient client;
+
+    private final ObjectMapper objectMapper;
+
+    public LiveBoardServiceImpl(OkHttpClient client, ObjectMapper objectMapper) {
+        this.client = client;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Optional<LiveBoard> getForStation(String id) {
+        try {
+            return Optional.of(fetchLiveBoard(id));
+        } catch (IOException e) {
+            throw new ClientException(e);
+        }
+    }
+
+    private LiveBoard fetchLiveBoard(String id) throws IOException {
+        Request request = new Request.Builder()
+                .url(URL.replace("${id}", id))
+                .build();
+
+        ResponseBody responseBody;
+        LiveBoard liveBoard;
+        try (Response response = client.newCall(request).execute()) {
+            responseBody = Objects.requireNonNull(response.body());
+            liveBoard = objectMapper.readValue(responseBody.string(), LiveBoard.class);
+        }
+
+        return liveBoard;
+    }
+}
